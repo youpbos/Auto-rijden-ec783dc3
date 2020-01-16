2059ef402044ccc0a7b7383e99c1653bbda0f2d3
youp bos <2107418@talnet.nl>
2020-01-16 14:50:22 +0100

    ....

---
+++ rijbewijs.php
@@ -0,0 +1,10 @@
+<?php
+echo  "hoe oud ben je?".PHP_EOL;
+$a = readline("");
+if($a > 16.5){
+    echo "Je mag beginnen met rijlessen!";
+}
+elseif($a < 16.5 ){
+    echo  "Helaas, je mag nog niet beginnen met rijlessen";
+}
+?>
